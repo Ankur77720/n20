1b19f3c960fcf73a133e3fe9d827a5ba3ae0bb79
Ankur77720 <prajapatiankur8349@gmail.com>
2024-10-19 13:08:29 +0530

    done

--- app.js
+++ app.js
@@ -1,46 +1,57 @@
-const express = require('express');
-const app = express();
-const fs = require('fs');
+const express = require('express')
+const fs = require('fs')
+const app = express()
+
+app.set('view engine', "ejs")
 
 
-app.set("view engine", "ejs");
+app.get('/', (req, res) => {
 
-
-
-app.get("/", (req, res) => {
-
-    fs.readdir('./notes', (err, data) => {
-
-        /* data = [ 'note1.txt', 'note2.txt' ] */
-
-        res.render("index", { notes: data })
+    fs.readdir('./notes', (err, files) => {
+        console.log(files)
+        res.render('index', { files })
     })
 
-});
-
-
+})
 
 app.get('/new', (req, res) => {
-    res.render("new")
+    res.render('new')
 })
 
 app.get('/new-note', (req, res) => {
 
-    fs.writeFile(`./notes/${req.query.title}.txt`, req.query.description, (err) => {
-        res.redirect("/")
+    const title = req.query.title
+    const description = req.query.description
+
+    fs.writeFile(`./notes/${title}.txt`, description, (err) => {
+        res.redirect('/')
     })
+
+    /* File create krna, file ke ander data likhna */
+
+
+
 })
+
+
+/* 
+/notes/note1.txt
+/notes/note2.txt
+ */
+
 
 app.get('/notes/:title', (req, res) => {
-    const title = req.params.title
 
+    const title = req.params.title
     console.log(title)
-    fs.readFile(`./notes/${title}`, "utf-8", (err, data) => {
-        res.render("noteDetail", { note: data })
+    fs.readFile(`./notes/${title}`, 'utf-8', (err, data) => {
+        res.send(data)
     })
 })
+
+
 
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+    console.log('server is running on port 3000')
+})
